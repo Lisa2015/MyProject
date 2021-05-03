41a8668115a25fc505bb01b43c8704dc219b3437
Lisa2015 <honor2105@gmail.com>
2021-05-03 23:40:55 +0800

    注册成功页面
    注册成功页面

--- laravel-test/app/Http/Controllers/UsersController.php
+++ laravel-test/app/Http/Controllers/UsersController.php
@@ -23,6 +23,15 @@ class UsersController extends Controller
            'email' => 'required|email|unique:users|max:255',
            'password' => 'required|confirmed|min:6'
        ]);
-       return;
+
+       $user = User::create([
+           'name' => $request->name,
+           'email'=> $request->email,
+           'password'=>bcrypt($request->password),
+       ]);
+       //可以使用 session() 方法来访问会话实例。而当我们想存入一条缓存的数据，让它只在下一次的请求内有效时，
+       //则可以使用 flash 方法。flash 方法接收两个参数，第一个为会话的键，第二个为会话的值，我们可以通过下面这行代码的为会话赋值。
+          session()->flash('success','欢迎，将在这里开启微博');
+       return redirect()->route('users.show',[$user]);
    }
 }
--- laravel-test/resources/views/user/create.blade.php
+++ laravel-test/resources/views/user/create.blade.php
@@ -10,6 +10,8 @@
     </div>
 
     <div class="card-body">
+     {{--      表单错误显示--}}
+      @include('shared._errors')
       <form method="POST" action="{{ route('users.store') }}">
         {{ csrf_field() }}
         <div class="form-group">
