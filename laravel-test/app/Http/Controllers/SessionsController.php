93c2b14f0e7c645f34ede58119d6bd0d5cbe1296
Lisa2015 <honor2105@gmail.com>
2021-05-04 21:30:53 +0800

    退出登录+记住登录状态

--- laravel-test/app/Http/Controllers/SessionsController.php
+++ laravel-test/app/Http/Controllers/SessionsController.php
@@ -18,13 +18,20 @@ class SessionsController extends Controller
             'email'=> 'required|email|max:255',
             'password' => 'required'
             ]);
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials,$request->has('remember'))) {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
             session()->flash('danger', '很抱歉，您登陆的用户不存在');
             return redirect()->back()->withInput();
         }
+    }
+
+    //用户退出登录
+    function  destroy() {
+        Auth::logout();
+        session()->flash('success','您已经成功退出！');
+        return redirect('login');
     }
 
 
