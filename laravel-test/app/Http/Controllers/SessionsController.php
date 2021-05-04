ed54bc518b352d92146dd46e34edd6ebc124deca
Lisa2015 <honor2105@gmail.com>
2021-05-04 14:45:06 +0800

    创建登录会话

--- laravel-test/app/Http/Controllers/SessionsController.php
+++ laravel-test/app/Http/Controllers/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
@@ -11,4 +12,20 @@ class SessionsController extends Controller
     public function create() {
         return view('sessions.create');
     }
+     //认证用户身份。对用户提交的数据进行验证。
+    public function store(Request  $request) {
+        $credentials = $this->validate($request,[
+            'email'=> 'required|email|max:255',
+            'password' => 'required'
+            ]);
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您登陆的用户不存在');
+            return redirect()->back()->withInput();
+        }
+    }
+
+
 }
